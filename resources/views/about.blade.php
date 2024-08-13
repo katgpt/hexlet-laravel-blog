1e34e64fcd3c26816a67cfcfb3a5d3034876bbff
siteservicehelp <siteservicehelp@gmail.com>
2024-08-13 08:42:05 +0600

    Layout step

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('title', 'О блоге')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+@section('header', 'О блоге')
 
-        <!-- Styles -->
-        <style></style>
-    </head>
-    <body>
-        <h1>О блоге</h1>
-        <p>Эксперименты с Laravel на Хекслете</p>
-    </body>
-</html>
+@section('content')
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
